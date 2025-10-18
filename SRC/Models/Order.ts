0f9e08518aadeaa4aa445b938bf016e25422f3e2
Kakada04 <kakada04as@gmail.com>
2025-10-18 12:57:22 +0700

    enableUploadsFOlder

--- SRC/Models/Order.ts
+++ SRC/Models/Order.ts
@@ -2,12 +2,16 @@ import { Schema, model, Document, Types } from 'mongoose';
 
 interface IOrderItem {
   productId: Types.ObjectId;
+  productName: string;        // Store product name at time of order
+  productCategory: string;    // Store category name at time of order
   quantity: number;
   price: number;
 }
 
 export interface IOrder extends Document {
   userId: Types.ObjectId;
+  userName: string;           // Store user name at time of order
+  userEmail: string;          // Store user email at time of order
   items: IOrderItem[];
   totalAmount: number;
   status: 'pending' | 'completed' | 'cancelled';
@@ -16,15 +20,21 @@ export interface IOrder extends Document {
 
 const orderSchema = new Schema<IOrder>({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  items: [
-    {
-      productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
-      quantity: { type: Number, required: true },
-      price: { type: Number, required: true }
-    }
-  ],
+  userName: { type: String, required: true },
+  userEmail: { type: String, required: true },
+  items: [{
+    productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+    productName: { type: String, required: true },
+    productCategory: { type: String, required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true }
+  }],
   totalAmount: { type: Number, required: true },
-  status: { type: String, enum: ['pending', 'completed', 'cancelled'], default: 'pending' },
+  status: { 
+    type: String, 
+    enum: ['pending', 'completed', 'cancelled'], 
+    default: 'completed' 
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
--- SRC/Routes/orders.routes.ts
+++ SRC/Routes/orders.routes.ts
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {createOrder, listOrders, getOrderById} = require('../Controllers/order.controller');
+const {createOrder, listOrders, getOrderById,getSalesByPeriod} = require('../Controllers/order.controller');
 const {protect,authorizeRoles} = require('../Middleware/auth');
 
 
 router.post('/',protect, createOrder);
 router.get('/',protect, authorizeRoles('admin'), listOrders);
 router.get('/:id',protect, getOrderById);
+router.get('/analytics/sales', protect, authorizeRoles('admin'), getSalesByPeriod);
 
 module.exports = router;
